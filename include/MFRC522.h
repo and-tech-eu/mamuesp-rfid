199580530133e6722b0e75d1e3f420626eba4256
Manfred Müller-Späth <fms1961@gmail.com>
2018-05-10 11:17:53 +0200

    Update MFRC522.h

--- include/MFRC522.h
+++ include/MFRC522.h
@@ -81,7 +81,7 @@
 #define __STDC_LIMIT_MACROS
 #include <stdint.h>
 #include <Arduino.h>
-#include <Wire.h>
+#include "SPI.h"
 
 // Firmware data for self-test
 // Reference values based on firmware version
